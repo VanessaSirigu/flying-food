356e2c5ee383db5d8d5b1b2a64067aac0b4baf43
Vanessa Sirigu <vane.sirigu@gmail.com>
2022-12-22 16:11:43 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Text } from './components/Text'
+// import { Text } from './components/Text'
+import { Cart } from './pages/Cart'
+import { Homepage } from './pages/Homepage'
+import { NotFound } from './pages/NotFound'
+import { ProductDetail } from './pages/ProductDetail'
+import { Products } from './pages/Products'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />
+  },
+  {
+    path: '/products',
+    element: <Products />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/products/:id',
+    element: <ProductDetail />
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+])
 
 function App() {
-  return (
-    <div>
-      <Text fontColor="green">Test</Text>
-      <Text fontColor="yellow">2</Text>
-      <Text fontColor="purple">4</Text>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
