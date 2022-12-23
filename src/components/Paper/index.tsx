19191465d62aabaf36552bb8c6685e22ba9f49e9
Vanessa Sirigu <vane.sirigu@gmail.com>
2022-12-23 14:36:58 +0100

    add Paper

---
+++ src/components/Paper/index.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react'
+import { theme, ThemeColor } from '../../style/theme'
+import { StyledPaper } from './styled'
+
+type Props = {
+  children: ReactNode
+  className?: string
+  bgColor?: ThemeColor
+  rounded?: boolean
+  shadow?: boolean
+}
+
+export const Paper = ({
+  children,
+  className,
+  rounded = true,
+  shadow = true,
+  bgColor = 'background'
+}: Props) => {
+  return (
+    <StyledPaper
+      bgColor={bgColor}
+      rounded={rounded}
+      shadow={shadow}
+      className={className}
+    >
+      {children}
+    </StyledPaper>
+  )
+}
---
+++ src/components/Paper/styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+import { ThemeColor } from '../../style/theme'
+
+export const StyledPaper = styled.div<{
+  bgColor: ThemeColor
+  className?: string
+  background?: boolean
+  rounded?: boolean
+  shadow?: boolean
+}>`
+  background-color: ${({ theme, bgColor }) => theme.colors[bgColor]};
+  border-radius: ${({ theme, rounded }) => rounded && theme.radii.at(1) + 'px'};
+  box-shadow: ${({ shadow }) => shadow && '0 8px 40px rgb(0, 0, 0, 0.1)'};
+`
