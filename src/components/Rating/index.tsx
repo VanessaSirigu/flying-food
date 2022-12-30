14164f0789e9135802f23c644830d1e349a9124a
Vanessa Sirigu <vane.sirigu@gmail.com>
2022-12-30 09:01:23 +0100

    add rating

---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,25 @@
+import { ThemeColor } from '../../style/theme'
+import { Icon } from '../Icon'
+import { StyledRating } from './styled'
+
+type Props = {
+  value: number
+  color?: ThemeColor
+  stars?: number
+}
+
+export const Rating = ({ value, color = 'primary', stars = 5 }: Props) => {
+  const getIcon = (index: number) => {
+    if (index + 1 <= value) return 'fullStar'
+    if (index + 0.5 <= value) return 'halfStar'
+    return 'emptyStar'
+  }
+
+  return (
+    <StyledRating>
+      {Array.from({ length: stars }).map((_, i) => (
+        <Icon name={getIcon(i)} color={color} key={i} />
+      ))}
+    </StyledRating>
+  )
+}
---
+++ src/components/Rating/styled.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const StyledRating = styled.div`
+  /* display: flex;
+  gap: 4px; */
+`
