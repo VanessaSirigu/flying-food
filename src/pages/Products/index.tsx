87913d739fa9a6f78ab0169b973ece0a6f53a1ff
Vanessa Sirigu <vane.sirigu@gmail.com>
2023-01-11 09:28:03 +0100

    add api data to product page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from 'react'
+import { getProducts } from '../../api'
+import { ProductDto } from '../../api/types'
+import { Loader } from '../../components/Loader'
+import { ProductCard } from '../../components/ProductCard'
+
 export const Products = () => {
-  return <div>Products page</div>
+  const [data, setData] = useState<ProductDto[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const getData = (prod: ProductDto[]) => {
+    setData(prod)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getProducts()
+      .then((prod) => {
+        getData(prod)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <div>
+      {loading && <Loader />}
+      {data && (
+        <div>
+          {data.map((p, i) => (
+            <ProductCard
+              key={i}
+              imgSrc={p.imageUrl}
+              name={p.name}
+              rating={p.rating}
+              price={`${p.price.type} ${p.price.value}`}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
