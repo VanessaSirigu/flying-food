f462606f748cff128851326d8e09e73da567f6fb
Vanessa Sirigu <vane.sirigu@gmail.com>
2023-01-12 12:20:30 +0100

    fix products filter

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import { getProducts, getTags } from '../../api'
 import { ProductDto, TagDto } from '../../api/types'
 import { FilterItem } from '../../components/Filter'
 import { Grid } from '../../components/Grid'
 import { Loader } from '../../components/Loader'
 import { ProductCard } from '../../components/ProductCard'
+import { ProductDetail } from '../ProductDetail'
 import { ProductsHeader } from './ProductsHeader'
 
 export const Products = () => {
   const [products, setProducts] = useState<ProductDto[]>()
   const [tags, setTags] = useState<TagDto[]>([])
   const [selected, setSelected] = useState('')
-  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     Promise.all([getTags(), getProducts()])
@@ -20,14 +21,15 @@ export const Products = () => {
         setProducts(products)
       })
       .catch((err) => console.log(err))
-      .finally(() => {
-        setLoading(false)
-      })
   }, [])
 
   const handleFilterClick = (tag: FilterItem) => {
     setSelected(tag.id === selected ? '' : tag.id)
   }
+
+  const filterProducts = selected
+    ? products?.filter(({ tags }) => tags.includes(selected))
+    : products
 
   return (
     <div>
@@ -36,20 +38,24 @@ export const Products = () => {
         items={tags.filter((tag) => !tag.hidden).map(({ name, id }) => ({ name, id }))}
         onFilterSelect={handleFilterClick}
       />
-      {loading && <Loader />}
-      {products && (
+      {!products && <Loader />}
+      {filterProducts && (
         <Grid>
-          {products
-            .filter(({ id, tags }) => tags.includes(selected))
-            .map((p, i) => (
+          {filterProducts.map((p, i) => (
+            <div>
+              <Routes>
+                <Route path="products:id" element={<ProductDetail />} />
+              </Routes>
               <ProductCard
                 key={i}
+                id={p.id}
                 imgSrc={p.imageUrl}
                 name={p.name}
                 rating={p.rating}
                 price={`${p.price.type} ${p.price.value}`}
               />
-            ))}
+            </div>
+          ))}
         </Grid>
       )}
     </div>
