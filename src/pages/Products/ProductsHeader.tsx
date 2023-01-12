bb572c9ddf64fdf125b0c0543ac8bd8cd81ac754
Vanessa Sirigu <vane.sirigu@gmail.com>
2023-01-12 09:08:56 +0100

    rename ProductsHeader

--- src/pages/Products/ProductsHeader.tsx
+++ src/pages/Products/ProductsHeader.tsx
@@ -4,15 +4,16 @@ import { StyledDiv, StyledButton } from './styled'
 
 type Props = {
   items: FilterItem[]
-  onSelect?: () => void
+  selected?: string
+  onFilterSelect: (selected: FilterItem) => void
 }
 
-export const ProdutsHeader = ({ items, onSelect }: Props) => {
+export const ProductsHeader = ({ items, selected, onFilterSelect }: Props) => {
   return (
     <StyledDiv>
       <SectionTitle main="our" secondary="products" />
-      <Filter items={items} onSelect={onSelect} />
-      <StyledButton>Go to catalog</StyledButton>
+      <Filter items={items} onSelect={onFilterSelect} selected={selected} />
+      <StyledButton bold>Go to catalog</StyledButton>
     </StyledDiv>
   )
 }
