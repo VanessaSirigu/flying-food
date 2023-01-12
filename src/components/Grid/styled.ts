3c87a744bb6ee2c6805b14772f4358e3244318a5
JacopoMarini <marinijacopo96@gmail.com>
2023-01-12 14:15:15 +0100

    Fix grid style

--- src/components/Grid/styled.ts
+++ src/components/Grid/styled.ts
@@ -3,8 +3,9 @@ import styled from '@emotion/styled'
 export const StyledGrid = styled.div`
   display: grid;
   max-width: ${({ theme }) => theme.containers.content}px;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 36px;
   grid-auto-rows: 340px;
   margin-left: 32px;
+  padding-top: 32px;
 `
