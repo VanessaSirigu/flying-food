e74cc33770e2abe44daf242bde0e714422a3ce82
Vanessa Sirigu <vane.sirigu@gmail.com>
2023-01-13 11:25:41 +0100

    add getRandomProducts

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,3 +6,5 @@ export const getProducts = () => apiClient.get<ProductDto[]>('products')
 export const getTags = () => apiClient.get<TagDto[]>('tags')
 
 export const getProductById = (id: string) => apiClient.get<ProductDto>(`products/${id}`)
+
+export const getRandomProducts = () => apiClient.get<ProductDto[]>('products/random')
