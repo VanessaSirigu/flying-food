c793bd7990f6d62cc9e1f14fb3e250da6b708af0
Vanessa Sirigu <vane.sirigu@gmail.com>
2023-01-13 10:16:47 +0100

    add QuantitySelector

--- src/components/QuantitySelector/index.tsx
+++ src/components/QuantitySelector/index.tsx
@@ -4,26 +4,33 @@ import { Text } from '../Text'
 
 type Props = {
   quantity: number
+  min?: number
+  max?: number
   onClick: (q: number) => void
 }
 
-export const QuantitySelector = ({ quantity, onClick }: Props) => {
+export const QuantitySelector = ({ quantity, onClick, min, max }: Props) => {
+  const onDecrement = () => {
+    if (typeof min !== 'number' || quantity - 1 >= min) {
+      onClick(-1)
+    }
+  }
+
+  const onIncrement = () => {
+    if (typeof max !== 'number' || quantity + 1 <= max) {
+      onClick(+1)
+    }
+  }
+
   return (
     <Stack centered>
-      <IconButton
-        outlined
-        bgColor="background"
-        icon="minus"
-        // onClick={() => (quantity > 0 ? setQuantity(quantity - 1) : setQuantity(0))}
-        onClick={() => onClick(-1)}
-      />
+      <IconButton outlined bgColor="background" icon="minus" onClick={onDecrement} />
       <Text>{quantity}</Text>
       <IconButton
         bgColor="backgroundDark"
         color="textInverse"
         icon="plus"
-        // onClick={() => setQuantity(quantity + 1)}
-        onClick={() => onClick(1)}
+        onClick={onIncrement}
       />
     </Stack>
   )
