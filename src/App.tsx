bf3f45b4df2793dad74b0e86c9215bf4eedc03cd
Vanessa Sirigu <vane.sirigu@gmail.com>
2023-01-13 11:24:17 +0100

    add BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Global, ThemeProvider } from '@emotion/react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Cart } from './pages/Cart'
 import { Homepage } from './pages/Homepage'
@@ -9,43 +9,23 @@ import { Products } from './pages/Products'
 import { globalStyle } from './style/global'
 import { theme } from './style/theme'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Homepage />
-      },
-      {
-        path: '/products',
-        element: <Products />
-      },
-      {
-        path: '/cart',
-        element: <Cart />
-      },
-      {
-        path: '/products/:id',
-        element: <ProductDetail />
-      },
-      {
-        path: '*',
-        element: <NotFound />
-      }
-    ]
-  }
-])
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Homepage />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/:id" element={<ProductDetail />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
-  // <Text>A</Text>
 }
 
 export default App
